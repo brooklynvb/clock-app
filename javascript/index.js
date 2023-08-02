782e6e6948fe936d55bced5c0e610fc8887e2672
ANurfHerder <bevanbebber@gmail.com>
2023-08-02 11:52:46 -0400

    Added JS for time to two locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,21 @@
-function showDate(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY h:mm A");
+function updateTime() {
+  //LA
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-    alert(`It is ${currentTime} in ${event.target.value}`);
-  }
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
-
-let selectElement = document.querySelector("#city");
-selectElement.addEventListener("change", showDate);
+updateTime();
+setInterval(updateTime, 1000);
