565b8e81f815fe9c9c2708817f8143eb88049b01
ANurfHerder <bevanbebber@gmail.com>
2023-08-04 17:16:16 -0400

    Changed base layout

--- javascript/index.js
+++ javascript/index.js
@@ -59,7 +59,6 @@ function updateCity(event) {
 
 updateTime();
 setInterval(updateTime, 1000);
-setInterval(updateCity, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
