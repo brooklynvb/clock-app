700d5dc74e47ff7cd2cae76361a420a5967ed31d
ANurfHerder <bevanbebber@gmail.com>
2023-08-04 16:00:48 -0400

    Updated CSS and added new cites

--- javascript/index.js
+++ javascript/index.js
@@ -21,6 +21,17 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 //Select City
@@ -42,11 +53,13 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+  <div class="home"><a href="/">Return to Home</a></div>
   `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+setInterval(updateCity, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
